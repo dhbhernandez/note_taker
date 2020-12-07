30ed69f2679cc702f390947257cfbd9922190eb0
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2020-12-06 22:49:25 -0600

    first push

---
+++ server.js
@@ -0,0 +1,25 @@
+// required modules
+const express = require("express");
+const fs = require("fs");
+
+// server application at port 
+const app = express();
+const PORT = process.env.PORT || 4800;
+
+// Read URL or JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Use public folder
+app.use(express.static("public"));
+
+// Included js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+// Add listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
